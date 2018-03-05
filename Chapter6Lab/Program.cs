fad100d189985c90864db8a7d2ac80752505cb17
LSLIM <LSLIM@LS>
2018-03-05 20:55:52 +0100

    I add the task as a comment

--- Chapter6Lab/Program.cs
+++ Chapter6Lab/Program.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter6Lab
-{
+{   /*In the application, create a class called Account with a method for debiting or credititing the total balance of an account. 
+    The program should prompt the end user to first enter the account details (account number and balance) and then the credit/debit amount. 
+    The prompt to enter the credit/debit amount should be in a loop and should execute till the end user wants.
+    Each time the end user enters the cred/debit amount, the program should also show the new balance. */
     class Program
     {
         static void Main(string[] args)
